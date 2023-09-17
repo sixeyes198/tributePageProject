adebf5e798737b5024a0e0d4ca7d236c2063933e
sixeyes198 <yossiguadie@icloud.com>
2023-09-17 09:50:14 +0300

    added click nav, fixed navbar

--- project-script.js
+++ project-script.js
@@ -6,3 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
         navbar.classList.toggle('active');
     });
 });
+const navLinks = document.querySelectorAll('.navbar a');
+navLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default link behavior
+        const targetId = link.getAttribute('href').substring(1); // Get the target section's id
+        const targetSection = document.getElementById(targetId); // Find the target section by id
+
+        if (targetSection) {
+            // Scroll to the target section smoothly
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+            });
+        }
+    });
+});
